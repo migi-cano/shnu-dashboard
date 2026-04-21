390bad052a37a08f269e86154c89454d4629b0fd
migi-cano <juanmiguelcano15@gmail.com>
2026-04-21 15:03:33 +0800

    Remove server folder from client branch

--- server/routes/supplierRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSuppliers, updateSupplier } = require('../controllers/supplierController');
-
-router.get('/', getSuppliers);
-router.post('/update', updateSupplier);
-
-module.exports = router;
--- server/routes/winsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getWins, saveWins } = require('../controllers/winsController');
-
-router.get('/', getWins);
-router.post('/', saveWins);
-
-module.exports = router;
